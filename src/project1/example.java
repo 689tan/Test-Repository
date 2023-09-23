ac73499b69d3e9fb35a44bf4171e53cc1b59406a
689tan <689tan@gmail.com>
2023-09-23 11:40:26 -0600

    first commit

---
+++ src/project1/example.java
@@ -0,0 +1,20 @@
+package project1;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 3 ;
+int y = 5 ;
+
+int temp = x ;
+x = y ;
+y = temp ;
+
+
+
+
+		
+	}
+
+}
